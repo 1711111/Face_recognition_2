8cf492db9ed45d63452df229666d1fac31c10a6d
1711111 <120311353+1711111@users.noreply.github.com>
2024-04-19 16:34:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+Webcam.set({
+    width: 700,
+    height: 350,
+    image_format: "png",
+    png_quality: 90
+});
+camera = document.getElementById("camera")
+Webcam.attach('#camera');
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        console.log("Step1");
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+        document.getElementById("camera").style.display = "none";
+        document.getElementById("result").style.display = "initial";
+    });
+}
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/kuge1PtfU/model.json',modelLoaded);
+console.log('ml5 version is ',ml5.version);
+function modelLoaded(){
+    console.log("model is loaded");
+}
+function gotResult(error,results){
+    if (error){
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = (results[0].confidence.toFixed(2))*100 +"%";
+    }
+
+}
